3cdd11ddd9c0e389fed7a526ec245775f5343ff3
Ryan Nielson <ryan.nielson@gmail.com>
2013-12-10 10:11:54 -0400

    Added newline match

--- lib/generators/remove_turbolinks/remove_generator.rb
+++ lib/generators/remove_turbolinks/remove_generator.rb
@@ -2,8 +2,8 @@ module RemoveTurbolinks
   module Generators
     class RemoveGenerator < Rails::Generators::Base
       def remove_from_gemfile
-        gsub_file "Gemfile", /# Turbolinks makes following links in your web application faster. Read more: https:\/\/github.com\/rails\/turbolinks/, ""
-        gsub_file "Gemfile", /gem 'turbolinks'/, ""
+        gsub_file "Gemfile", /# Turbolinks makes following links in your web application faster. Read more: https:\/\/github.com\/rails\/turbolinks[\r\n]/, ""
+        gsub_file "Gemfile", /gem 'turbolinks'[\r\n]/, ""
       end
 
       def remove_from_application_layout
@@ -11,7 +11,7 @@ module RemoveTurbolinks
       end
 
       def remove_from_application_javascript
-        gsub_file "app/assets/javascripts/application.js", /\/\/= require turbolinks/, ""
+        gsub_file "app/assets/javascripts/application.js", /\/\/= require turbolinks[\r\n]/, ""
       end
     end
   end
